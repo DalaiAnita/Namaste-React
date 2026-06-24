bb7b5af180624056a4ada354ea3bb9d28e352878
DalaiAnita <anitadalai9589@gmail.com>
2026-06-24 23:23:57 +0530

    React Basics

--- App.js
+++ App.js
@@ -2,21 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div", {id: "child1"},[
-        React.createElement("h1",{},"I am an h1 tag!"),
-        React.createElement("h2",{},"I am an h2 tag! from React")
-    ]),
-     React.createElement("div", {id: "child2"},[
-        React.createElement("h1",{},"I am an h1 tag!"),
-        React.createElement("h2",{},"I am an h2 tag!")
-    ])
-])
 
 
-// const heading = React.createElement("h1", 
-//     {id:"heading"}, "Hello world from React!");
+
+const JSXheading = <h1>Heading from JSX </h1>
 
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root.render(parent);
+    root.render(JSXheading);
