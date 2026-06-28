fe1bf65961804af495089afced4f3b7ba9274eb5
DalaiAnita <anitadalai9589@gmail.com>
2026-06-28 23:34:42 +0530

    Swiggy API data display in card

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
-
-
+import ReactDOM from "react-dom/client";
 
 // //using React.createElement
 // const header1 = React.createElement("div",{className: "title"},[
@@ -21,7 +19,7 @@ import ReactDOM from "react-dom/client"
 
 // //using functional component
 // const Paragraph = () => {
-//     return (<p>Hello I am a pragraph and i will 
+//     return (<p>Hello I am a pragraph and i will
 //         be used inside the Functional header component and it is called component cpomposition</p>)
 // }
 
@@ -47,8 +45,8 @@ import ReactDOM from "react-dom/client"
 //           />
 //         </div>
 //         <div className="search-container">
-//             <input 
-//               type="text" 
+//             <input
+//               type="text"
 //               placeholder="Search here.."
 //             />
 //         </div>
@@ -87,53 +85,356 @@ import ReactDOM from "react-dom/client"
 // * ----Address
 // * ----Contact
 
-const Header = () =>{
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src="https://img.freepik.com/premium-vector/good-food-logo-design_79169-10.jpg?w=2000"
-                alt="good food" />
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://img.freepik.com/premium-vector/good-food-logo-design_79169-10.jpg?w=2000"
+          alt="good food"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = ({resData}) => {
+  console.log("resData---->", resData);
+  const {name, cuisines, avgRating, costForTwo, cloudinaryImageId, sla} = resData;
+  return (
+    <div className="card">
+      <img
+        className="card-img"
+        src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}
+        alt="restaurant"
+      />
+
+      <div className="card-body">
+        <h3>{name}</h3>
+        <div className="card-info">
+          <span className="rating">⭐ {avgRating} </span>
+          <span className="delivery-time">⏱️ {sla.deliveryTime} mins</span>
         </div>
-    )
-}
+        <p className="cuisines">{cuisines.join(',')}</p>
 
-const RestaurantCard = (props) =>{
-    return (
-        <div className="card">
-            <img 
-                className="card-img"
-                src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4"
-                alt="restaurant"/>
-
-                <div className="card-body">
-                    <h3>{props.resName}</h3>
-
-
-                    <div className="card-info">
-                        <span className="rating">⭐ {props.rating} </span>
-                        <span className="delivery-time">⏱️ {props.time} mins</span>
-                    </div>
-
-                     <p className="cuisines">
-                        {props.cuisine}
-                    </p>
-
-                    <p className="price">₹{props.price} for two</p>
-                </div>
-
-
-        </div>
-    )
-}
+        <p className="price">{costForTwo}</p>
+      </div>
+    </div>
+  );
+};
+const resObj = {
+  restaurants: [
+    {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+      info: {
+        id: "10583",
+        name: "Pizza Hut",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2026/6/15/42eb0361-ebd7-4570-affe-a6894ea0d2ee_10583.JPG",
+        locality: "Coles Road",
+        areaName: "Frazer Town",
+        costForTwo: "₹350 for two",
+        cuisines: ["Pizzas"],
+        avgRating: 4.2,
+        parentId: "721",
+        avgRatingString: "4.2",
+        totalRatingsString: "20K+",
+        sla: {
+          deliveryTime: 29,
+          lastMileTravel: 3.9,
+          serviceability: "SERVICEABLE",
+          slaString: "25-30 mins",
+          lastMileTravelString: "3.9 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2026-06-28 03:00:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {},
+            textBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "70% OFF",
+          subHeader: "UPTO ₹130",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "4.4",
+            ratingCount: "3.0K+",
+          },
+          source: "GOOGLE",
+          sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {
+        context: "seo-data-9ae85248-2089-41b3-9475-2a6f68925628",
+      },
+      cta: {
+        link: "https://www.swiggy.com/city/bangalore/pizza-hut-coles-road-frazer-town-rest10583",
+        text: "RESTAURANT_MENU",
+        type: "WEBLINK",
+      },
+      widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+    },
+    {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+      info: {
+        id: "518369",
+        name: "Madras Meal Company",
+        cloudinaryImageId: "bqy6rjw55ctn04drgtt2",
+        locality: "Majestic",
+        areaName: "Malleshwaram",
+        costForTwo: "₹350 for two",
+        cuisines: ["South Indian"],
+        avgRating: 4.6,
+        parentId: "128893",
+        avgRatingString: "4.6",
+        totalRatingsString: "339",
+        sla: {
+          deliveryTime: 26,
+          lastMileTravel: 2.4,
+          serviceability: "SERVICEABLE",
+          slaString: "25-30 mins",
+          lastMileTravelString: "2.4 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2026-06-28 01:00:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {},
+            textBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "ITEMS",
+          subHeader: "AT ₹99",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "--",
+          },
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {
+        context: "seo-data-9ae85248-2089-41b3-9475-2a6f68925628",
+      },
+      cta: {
+        link: "https://www.swiggy.com/city/bangalore/madras-meal-company-majestic-malleshwaram-rest518369",
+        text: "RESTAURANT_MENU",
+        type: "WEBLINK",
+      },
+      widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+    },
+    {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+      info: {
+        id: "337335",
+        name: "Kannur food kitchen",
+        cloudinaryImageId: "a27weqanhnszqiuzsoik",
+        locality: "1st  Stage",
+        areaName: "BTM Layout",
+        costForTwo: "₹200 for two",
+        cuisines: ["Kerala", "Biryani", "Beverages"],
+        avgRating: 4.2,
+        parentId: "114756",
+        avgRatingString: "4.2",
+        totalRatingsString: "25K+",
+        sla: {
+          deliveryTime: 36,
+          lastMileTravel: 8.9,
+          serviceability: "SERVICEABLE",
+          slaString: "35-45 mins",
+          lastMileTravelString: "8.9 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2026-06-28 04:30:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {},
+            textBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "50% OFF",
+          discountTag: "FLAT DEAL",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "--",
+          },
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {
+        context: "seo-data-9ae85248-2089-41b3-9475-2a6f68925628",
+      },
+      cta: {
+        link: "https://www.swiggy.com/city/bangalore/kannur-food-kitchen-1st-stage-btm-layout-rest337335",
+        text: "RESTAURANT_MENU",
+        type: "WEBLINK",
+      },
+      widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+    },
+    {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+      info: {
+        id: "337336",
+        name: "Bhillai food kitchen",
+        cloudinaryImageId: "a27weqanhnszqiuzsoik",
+        locality: "1st  Stage",
+        areaName: "BTM Layout",
+        costForTwo: "₹200 for two",
+        cuisines: ["Kerala", "Biryani", "Beverages"],
+        avgRating: 4.2,
+        parentId: "114757",
+        avgRatingString: "4.2",
+        totalRatingsString: "25K+",
+        sla: {
+          deliveryTime: 36,
+          lastMileTravel: 8.9,
+          serviceability: "SERVICEABLE",
+          slaString: "35-45 mins",
+          lastMileTravelString: "8.9 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2026-06-28 04:30:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {},
+            textBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "50% OFF",
+          discountTag: "FLAT DEAL",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+        externalRatings: {
+          aggregatedRating: {
+            rating: "--",
+          },
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      },
+      analytics: {
+        context: "seo-data-9ae85248-2089-41b3-9475-2a6f68925628",
+      },
+      cta: {
+        link: "https://www.swiggy.com/city/bangalore/kannur-food-kitchen-1st-stage-btm-layout-rest337335",
+        text: "RESTAURANT_MENU",
+        type: "WEBLINK",
+      },
+      widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+    }
+  ],
+};
 
 const Body = () => {
   return (
@@ -143,75 +444,21 @@ const Body = () => {
         <button>Search</button>
       </div>
       <div className="res-container">
-        <RestaurantCard 
-            resName="Burger King" 
-            cuisine="Burgers, Fast Food, Beverages" 
-            time="25"
-            price="300"
-            rating="4.3"
-            />
-        <RestaurantCard 
-            resName="KFC" 
-            cuisine="Burgers, Fast Food, Beverages" 
-            time="35"
-            price="500"
-            rating="4.5"
-            />
-        <RestaurantCard 
-            resName="WOW Momos" 
-            cuisine="Fried Momos, Tandoori Momos " 
-            time="20"
-            price="350"
-            rating="4.4"
-            />
-        <RestaurantCard 
-            resName="Paradise" 
-            cuisine="Dum Biryani, Veg Biryani " 
-            time="30"
-            price="450"
-            rating="4.1"
-            />
-            <RestaurantCard 
-            resName="Burger King" 
-            cuisine="Burgers, Fast Food, Beverages" 
-            time="25"
-            price="300"
-            rating="4.3"
-            />
-        <RestaurantCard 
-            resName="KFC" 
-            cuisine="Burgers, Fast Food, Beverages" 
-            time="35"
-            price="500"
-            rating="4.5"
-            />
-        <RestaurantCard 
-            resName="WOW Momos" 
-            cuisine="Fried Momos, Tandoori Momos " 
-            time="20"
-            price="350"
-            rating="4.4"
-            />
-        <RestaurantCard 
-            resName="Paradise" 
-            cuisine="Dum Biryani, Veg Biryani " 
-            time="30"
-            price="450"
-            rating="4.1"
-            />
+        {resObj.restaurants.map((restaurant) => (
+        <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
+        ))}
       </div>
     </div>
   );
 };
-const AppLayout = () =>{
-    return (
-        <div className="app">
-            <Header />
-            <Body />
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        </div>
-    )
-}
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-
-    root.render(<AppLayout />);
+root.render(<AppLayout />);
