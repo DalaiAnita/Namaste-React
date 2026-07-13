752bec956fda15d00bb75637fd968a3a284bfbe3
DalaiAnita <anitadalai9589@gmail.com>
2026-07-13 23:29:12 +0530

    API fetch in Class component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,43 @@
-import User from "./User";
+// import User from "./User";
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
+// const About = () => {
+//     return (
+//        <div>
+//         <h1>About Us Page...</h1>
+//         {/* <User name={"Anita Singh"} age={"26"} location={"Bangalore"} /> */}
+//         <UserClass name={"Anita Dalai"} age={"26"} location={"Bangalore"} />
+//        </div>
+//     )
+// }
+
+// export default About;
+
+
+class About extends Component {
+    constructor(props){
+        super(props);
+        console.log("parent Constructor is called");
+    }
+
+    componentDidMount(){
+        console.log("parent Component Did mount is called");
+    }
+
+render(){
+    console.log("parent Render is called")
     return (
-       <div>
-        <h1>About Us Page...</h1>
-        {/* <User name={"Anita Singh"} age={"26"} location={"Bangalore"} /> */}
-        <UserClass name={"Anita Dalai"} age={"26"} location={"Bangalore"} />
-       </div>
+        <div>
+            <h1>Class based About page</h1>
+            <UserClass name={"First Child"} age={"26"} location={"Bangalore"} />
+            <UserClass name={"Second Child"} age={"26"} location={"Bangalore"} />
+            <UserClass name={"Third Child"} age={"26"} location={"Bangalore"} />
+            <UserClass name={"Forth Child"} age={"26"} location={"Bangalore"} />
+
+        </div>
     )
+}
 }
 
 export default About;
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,30 +1,43 @@
 import React from "react";
 
-
 class UserClass extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            count: 0,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
 
-    render(){
-        const {name, age, location} = this.props;
-        const {count} = this.state;
-        return (
-          <div>
-            <h1>User from Class component</h1>
-            <h5>Name: {name}</h5>
-            <h5>Age: {age}</h5>
-            <h5>Location: {location}</h5>
-            <h5>Count: {count}</h5>
-            <button onClick={() =>{
-                this.setState({count : count + 1})
-            }}>Increse Count</button>
-          </div>
-        );
-    }
+    console.log(this.props.name + " Constructor is called");
+  }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/octocat/followers");
+    const responseData = await data.json();
+    console.log("response", responseData);
+    console.log(this.props.name + "Component Did mount");
+  }
+
+  render() {
+    console.log(this.props.name + "Render is called");
+    const { name, age, location } = this.props;
+    const { count } = this.state;
+    return (
+      <div>
+        <h2>User from Class component</h2>
+        <h5>Name: {name}</h5>
+        <h5>Age: {age}</h5>
+        <h5>Location: {location}</h5>
+        <h5>Count: {count}</h5>
+        <button
+          onClick={() => {
+            this.setState({ count: count + 1 });
+          }}
+        >
+          Increse Count
+        </button>
+      </div>
+    );
+  }
 }
 
 export default UserClass;
