591f6f594151d9714627f04e3e44fc5414602fd6
DalaiAnita <anitadalai9589@gmail.com>
2026-07-17 22:55:21 +0530

    Fixed dummy API issue

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,6 +3,7 @@ import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../Utils/hooks/useOnlineStatus";
 
 const Body = () => {
   const [allRestaurants, setALlRestaurants] = useState([]);
@@ -17,7 +18,7 @@ const Body = () => {
   const fetchData = async () => {
     const responseData = await fetch("https://dummyjson.com/recipes");
     const data = await responseData.json();
-    const finalData =  data.recipes;
+    const finalData = data.recipes;
     setALlRestaurants(finalData);
     setFilterredRestaurants(finalData);
   };
@@ -26,57 +27,70 @@ const Body = () => {
 
   const searchRestaurants = () => {
     const filteredData = allRestaurants.filter((restaurant) => {
-      return restaurant?.info?.name.toLowerCase().includes(searchText.toLowerCase());
+      return restaurant?.name
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
     });
     setFilterredRestaurants(filteredData);
   };
 
-  return allRestaurants.length === 0 ? <Shimmer /> : 
-   (
-    <div className="body">
-      <div className="search-container">
-        <input
-          type="text"
-          value={searchText}
-          onChange={(event) => {
-            const text = event.target.value;
-            setSearchText(text);
-            searchRestaurants(text);
-          }}
-          placeholder="Search Recipes..."
-        />
+  const onlineStatus = useOnlineStatus();
 
-        <button onClick={searchRestaurants}>Search</button>
+  if (onlineStatus === false) {
+    return (
+      <h1>
+        Looks like you are offline...Please check your internet connection
+      </h1>
+    );
+  }
+    return allRestaurants.length === 0 ? (
+      <Shimmer />
+    ) : (
+      <div className="body">
+        <div className="search-container">
+          <input
+            type="text"
+            value={searchText}
+            onChange={(event) => {
+              const text = event.target.value;
+              setSearchText(text);
+              searchRestaurants(text);
+            }}
+            placeholder="Search Recipes..."
+          />
 
-        <button
-          onClick={() => {
-            const filteredRestaurantListData = allRestaurants.filter(
-              (restaurant) => restaurant?.info?.avgRating > 4.2,
-            );
-            setFilterredRestaurants(filteredRestaurantListData);
-          }}
-        >
-          Top Rated Restaurants
-        </button>
+          <button onClick={searchRestaurants}>Search</button>
 
-        <button
-          onClick={() => {
-            setSearchText("");
-            setFilterredRestaurants(allRestaurants);
-          }}
-        >
-          Reset
-        </button>
+          <button
+            onClick={() => {
+              const filteredRestaurantListData = allRestaurants.filter(
+                (restaurant) => restaurant?.rating > 4.7,
+              );
+              setFilterredRestaurants(filteredRestaurantListData);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+
+          <button
+            onClick={() => {
+              setSearchText("");
+              setFilterredRestaurants(allRestaurants);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+
+        <div className="res-container">
+          {filteredRestaurants.map((rest) => (
+            <Link to={`/restaurant/${rest.id}`} key={rest.id}>
+              <RestaurantCard restData={rest} />
+            </Link>
+          ))}
+        </div>
       </div>
-
-      <div className="res-container">
-        {filteredRestaurants.map((rest) => (
-          <Link to={`/restaurant/${rest.id}`}  key={rest.id} ><RestaurantCard restData={rest} /></Link>
-        ))}
-      </div>
-    </div>
-  );
-
+    );
 };
 
 export default Body;
