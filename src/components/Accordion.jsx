ba4acdfe505b1f98f84ea4777e74da184c2e2f11
DalaiAnita <anitadalai9589@gmail.com>
2026-07-27 23:51:07 +0530

    Controlled component

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,14 +1,27 @@
-const Accordion = ({ title, isOpen, onClick, children }) => {
+const Accordion = ({ title, isOpen, setShowIndex, children }) => {
+
+  const handleClick = () => {
+    setShowIndex();
+  };
+
   return (
     <div className="border rounded-lg my-4">
+
       <div
-        className="flex justify-between item-center p-4 cursor-pointer"
-        onClick={onClick}
+        className="flex justify-between items-center p-4 cursor-pointer"
+        onClick={handleClick}
       >
         <h2 className="font-bold">{title}</h2>
+
         <span>{isOpen ? "⬆️" : "⬇️"}</span>
       </div>
-      {isOpen && (<div className="p-4 border-t">{children}</div>)}
+
+      {isOpen && (
+        <div className="p-4 border-t">
+          {children}
+        </div>
+      )}
+
     </div>
   );
 };
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -5,24 +5,44 @@ import Shimmer from "./Shimmer";
 import Accordion from "./Accordion";
 
 const RestaurantMenu = () => {
+
   const { id } = useParams();
 
-  const [openAccordion, setOpenAccordion] = useState(null);
+  const [showItem, setShowItem] = useState(null);
 
-  const menuData = useRestaurantMenu(id);
+    const menuData = useRestaurantMenu(id);
+
+
+
+if (menuData === null) {
+  return <Shimmer />;
+}
+
+  const sections = [
+    {
+      title: "Instructions",
+      content: menuData.instructions,
+    },
+    {
+      title: "Ingredients",
+      content: menuData.ingredients,
+    },
+    {
+      title: "Tags",
+      content: menuData.tags,
+    },
+  ];
 
   const handleAccordionClick = (section) => {
     setOpenAccordion((prev) => (prev === section ? null : section));
   };
 
-  return menuData === null ? (
-    <Shimmer />
-  ) : (
+  return (
     <div className=" bg-white shadow-lg">
       <Accordion
         title="Instructions"
-        isOpen={openAccordion === "Instructions"}
-        onClick={() => handleAccordionClick("Instructions")}
+        isOpen={showItem === 0}
+        setShowIndex={() => setShowItem(0)}
       >
         {menuData.instructions.map((step, index) => (
           <p className="p-2" key={index}>
@@ -32,8 +52,8 @@ const RestaurantMenu = () => {
       </Accordion>
       <Accordion
         title="Ingredients"
-        isOpen={openAccordion === "Ingredients"}
-        onClick={() => handleAccordionClick("Ingredients")}
+        isOpen={showItem === 1}
+        setShowIndex={() => setShowItem(1)}
       >
         {menuData.ingredients.map((step, index) => (
           <p className="p-2" key={index}>
@@ -42,13 +62,18 @@ const RestaurantMenu = () => {
         ))}
       </Accordion>
 
-      <Accordion title="Tags" isOpen={openAccordion === "Tags"} onClick={() => handleAccordionClick("Tags")}>
+      <Accordion title="Tags" 
+        isOpen={showItem === 2}
+        setShowIndex={() => setShowItem(2)}
+      >
         {menuData.tags.map((step, index) => (
           <p className="p-2 m-2" key={index}>
             {index + 1}.{step}
           </p>
         ))}
       </Accordion>
+
+      
     </div>
   );
 };
